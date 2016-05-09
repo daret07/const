0c1dc243ece1b9a3a63c812310c51cc06a4a4141
jorge daret caravantes <Daret@MacBook-Pro-de-jorge.local>
2016-05-09 10:57:54 -0500

    se cambian las rutas para producción

--- nana/settings.py
+++ nana/settings.py
@@ -81,6 +81,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'practice.db', # Just the name
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',             
+        'PORT': '',   
     }
 }
 
